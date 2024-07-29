a1e5f259b3e0a3922dd05e0050513d98f5df099e
Gowtham3759 <gowtham3758>
2024-07-29 21:43:43 +0530

    payment page added with fields such as name,card number,expiry date,Cvv

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterForm from './Components/RegisterForm';
 import LoginForm from './Components/LoginForm';
 import Dashboard from './Components/Dashboard'; 
+import Payment from './Components/Payments';
+import Contact from './Components/Contact';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} /> 
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<LoginForm />} /> 
+    //     <Route path="/login" element={<LoginForm />} />
+    //     <Route path="/register" element={<RegisterForm />} />
+    //     <Route path="/dashboard" element={<Dashboard />} />
 
-      </Routes>
-    </Router>
+    //   </Routes>
+    // </Router>
+    <Contact/>
   );
 };
 

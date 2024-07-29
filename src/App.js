32f12725b8db1d765d2744e3372306298f1e97fc
harishragavendar <harishragavendar1202@gmail.com>
2024-07-29 14:04:53 +0530

    flipbook fix

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import RegisterForm from './Components/RegisterForm';
 import LoginForm from './Components/LoginForm';
 import Animations from './Components/Animations';
 import Storybook from './Components/Storybook';
+import Flippy from './Components/Flipbooks/Flippy';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/animation" element={<Animations />} />
-        <Route path="/flipbook" element={<Storybook />} />
+    <Flippy />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<LoginForm />} />
+    //     <Route path="/login" element={<LoginForm />} />
+    //     <Route path="/register" element={<RegisterForm />} />
+    //     <Route path="/animation" element={<Animations />} />
+    //     <Route path="/flipbook" element={<Storybook />} />
         
-      </Routes>
-    </Router>
+    //   </Routes>
+    // </Router>
   );
 }
 
